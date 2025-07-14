4ba63519a128b9f7c2a3bf72c5ff9b8eb6635a89
mb <ykf@live.cn>
2025-07-14 19:10:43 +0800

    update

--- distill/BAK/_2_DataDistiller_v9.py
+++ distill/BAK/_2_DataDistiller_v9.py
@@ -33,7 +33,7 @@ install()
 
 from utils.misc_utils import get_data_dir, get_project_root
 from utils.prompt_template import get_prompt_template
-from data_models.sft_data_models import (
+from data_classes.sft_data_models import (
     ClusterInfo,
     RawConstellationDataModel,
     SatelliteClusterOutput,
@@ -47,7 +47,7 @@ load_dotenv(env_path)
 
 # 获取Gemini API配置
 api_base_gemini = os.getenv("GEMINI_API_BASE")
-api_key_gemini = os.getenv("GEMINI_API_KEY_8")
+api_key_gemini = os.getenv("GEMINI_API_KEY_6")
 
 print(f"Gemini API配置: {'✓' if api_key_gemini and api_base_gemini else '✗'}")
 
@@ -463,7 +463,7 @@ def main():
     """主函数。"""
     # 从JSON文件加载数据
     input_file = (
-        get_data_dir() / "mock_satellite_observation_data_20250630_090758_v11.json"
+        get_data_dir() / "mock_satellite_observation_data_20250630_090754_v9.json"
     )
     batch_data = load_json_data(input_file)
 
